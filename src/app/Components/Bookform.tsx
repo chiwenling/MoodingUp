1891d131be9cb49d8270b335dff2412224cccfc8
chiwenling <147138397+chiwenling@users.noreply.github.com>
2024-08-21 15:36:14 +0800

    新增預約多筆資料功能

--- src/app/Components/Bookform.tsx
+++ src/app/Components/Bookform.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { setDoc, doc } from 'firebase/firestore';
+import { setDoc, doc, collection, addDoc} from 'firebase/firestore';
 import { db } from './../../../firebase';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../../lib/features/auth/authSlice';
@@ -10,7 +10,6 @@ export default function Bookform(){
     const router = useRouter();
     const user = useSelector(selectCurrentUser);
     const [formData, setFormData] = useState({
-        id:"",
         teacher: '',
         topic: '',
         date: '',
@@ -18,31 +17,35 @@ export default function Bookform(){
         radio: '',
     });
 
-    const handleChange = (e:any) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-        }));
+    const handleChange = function(e:any){
+        // name 取 formdata 裡的項目
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData(function(prevData){
+          return {
+            ...prevData, 
+            [name]:value
+        }
+      });
     };
 
     const handleSubmit = async (e:any) => {
         e.preventDefault();
-
         if (user && user.email) {
-        try {
-            await setDoc(doc(db, 'reservation', user.uid), {
-            email: user.email,
-            ...formData,
-            },
-            { merge: false }
-        );
-            alert('預約成功');
-            router.push("/profile");
-        } catch (error) {
-            console.error('Error saving data to Firestore:', error);
-            alert('請重新預約');
-        }
+          try {
+              const docRef = await addDoc(collection(db, "reservation"), {
+              ...formData,
+              user:user.uid,
+              email:user.email
+              });
+              console.log("id:", docRef.id)
+              alert("預約成功");
+              router.push("/profile");
+
+          } catch (error) {
+              console.error('Error saving data to Firestore:', error);
+              alert("請重新預約");
+          }
         }
     };
 
@@ -50,22 +53,22 @@ export default function Bookform(){
     <div className="flex items-center justify-center p-12">
       <div className="mx-auto w-full max-w-[550px]">
         <form onSubmit={handleSubmit}>
-        <div className="mb-5">
-            <label htmlFor="teacher" className="mb-3 block text-base font-medium text-[#07074D]">
+        <div className="-mx-1 px-1 flex flex-wrap w-full sm:w-1/2 mb-5">
+            <label htmlFor="teacher" className="mb-3 block text-base font-medium text-sisal-800">
                 指定預約老師
             </label>
-            <select name="teacher" id="teacher" value={formData.teacher} onChange={handleChange} required className="w-full appearance-none rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-border-sisal-900 focus:shadow-md">
+            <select name="teacher" id="teacher" value={formData.teacher} onChange={handleChange} required className="w-full appearance-none rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-sisal-800 outline-none focus:border-border-sisal-900 focus:shadow-md">
                 <option value="" disabled>請選擇一位老師</option>
                 <option value="陳比約">陳比約</option>
                 <option value="林得中">林得中</option>
                 <option value="蕭凱琳">蕭凱琳</option>
             </select>
         </div>
-        <div className="mb-5">
-            <label htmlFor="topic" className="required mb-3 block text-base font-medium text-[#07074D]">
+        <div className="-mx-1 px-1 flex flex-wrap w-full sm:w-1/2 mb-5">
+            <label htmlFor="topic" className="required mb-3 block text-base font-medium text-sisal-800">
                 討論諮詢主題
             </label>
-            <select name="topic" id="topic" value={formData.topic} onChange={handleChange} className="w-full appearance-none rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-border-sisal-900 focus:shadow-md"
+            <select name="topic" id="topic" value={formData.topic} onChange={handleChange} className="w-full appearance-none rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-sisal-800 outline-none focus:border-border-sisal-900 focus:shadow-md"
             >
                 <option value="" disabled>請選擇諮詢主題</option>
                 <option value="人際關係">人際關係</option>
@@ -76,10 +79,10 @@ export default function Bookform(){
           <div className="-mx-3 flex flex-wrap">
             <div className="w-full px-3 sm:w-1/2">
               <div className="mb-5">
-                <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
+                <label htmlFor="email" className="mb-3 block text-base font-medium text-sisal-800">
                   您的帳號
                 </label>
-                <input type="email" value={user?.email || ''} readOnly className="w-full rounded-md border py-3 px-6 text-base font-medium text-[#6B7280] border-sisal-400  p-2 pl-4 rounded-lg bg-gray-100 text-gray-900"/>
+                <input type="email" value={user?.email || ''} readOnly className="w-full rounded-md border py-3 px-6 text-base font-medium text-sisal-800 border-sisal-400  p-2 pl-4 rounded-lg bg-gray-100 text-gray-900"/>
               </div>
             </div>
           </div>
@@ -88,49 +91,31 @@ export default function Bookform(){
           <div className="-mx-3 flex flex-wrap">
             <div className="w-full px-3 sm:w-1/2">
               <div className="mb-5">
-                <label htmlFor="date" className="mb-3 block text-base font-medium text-[#07074D]">
+                <label htmlFor="date" className="mb-3 block text-base font-medium text-sisal-800">
                   日期
                 </label>
-                <input required type="date" name="date" id="date" value={formData.date} onChange={handleChange} className="w-full rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-border-sisal-900 focus:shadow-md"/>
+                <input required type="date" name="date" id="date" value={formData.date} onChange={handleChange} className="w-full rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-sisal-800 outline-none focus:border-border-sisal-900 focus:shadow-md"/>
               </div>
             </div>
           </div>
             <div className="-mx-3 flex flex-wrap">
                 <div className="w-full px-3 sm:w-1/2">
                     <div className="mb-5">
-                        <label htmlFor="time" className="mb-3 block text-base font-medium text-[#07074D]">
+                        <label htmlFor="time" className="mb-3 block text-base font-medium text-sisal-800">
                         時間
                         </label>
-                        <input
-                        type="time"
-                        name="time"
-                        id="time"
-                        value={formData.time}
-                        onChange={handleChange}
-                        className="w-full rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-border-sisal-900 focus:shadow-md"
-                        />
+                        <input type="time" name="time" id="time" value={formData.time} onChange={handleChange} className="w-full rounded-md border border-sisal-400 bg-white py-3 px-6 text-base font-medium text-sisal-800 outline-none focus:border-border-sisal-900 focus:shadow-md"/>
                     </div>
                 </div>
             </div>
 
           <div className="mb-5 flex items-center space-x-4">
-            <label className="text-base font-medium text-[#07074D]">
+            <label className="text-base font-medium text-sisal-800">
                 已詳閱預約規則
             </label>
             <div className="flex items-center">
-                <input
-                type="radio"
-                name="radio"
-                id="radioButton1"
-                value="Yes"
-                checked={formData.radio === 'Yes'}
-                onChange={handleChange}
-                className="h-5 w-5 text-[#6A64F1] focus:ring-0 border-gray-300 rounded-full"
-                />
-                <label
-                htmlFor="radioButton1"
-                className="pl-3 text-base font-medium text-[#07074D]"
-                >
+                <input type="radio" name="radio" id="radioButton" value="已詳閱" checked={formData.radio === "已詳閱"} onChange={handleChange} className="h-5 w-5 text-[#6A64F1] focus:ring-0 border-gray-300 rounded-full"/>
+                <label htmlFor="radioButton" className="pl-3 text-base font-medium text-sisal-800">
                 是
                 </label>
             </div>
