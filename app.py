5a4f9448425d335e6558e6690c81f8f4224a35df
chiwenling <147138397+chiwenling@users.noreply.github.com>
2024-08-28 22:46:50 +0800

    後台更新

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000","https://moodingup.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
