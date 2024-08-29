f54d5a78530487ffbb5d5f4ca87fd77cb456f717
chiwenling <147138397+chiwenling@users.noreply.github.com>
2024-08-29 18:21:50 +0800

    版本調整

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://mooding-up.vercel.app"],  
+    allow_origins=["http://localhost:3000","https://mooding-up.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
@@ -32,24 +32,25 @@ system_message = {
     "content": "你是溫柔的輔導老師，請用一次單元型模型，來鼓勵與你談話的人，給他們方向和建議，每次談話不超過20個字。"
 }
 
+
 class AiTalk:
     def __init__(self):
         self.messages = [system_message]
 
     def ai(self, prompt: str) -> str:
         self.messages.append({"role": "user", "content": prompt})
-        
+
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=self.messages
             )
-            reply = response.choices[0].message['content'] 
+            reply = response.choices[0].message['content']
             self.messages.append({"role": "assistant", "content": reply})
             return reply
         except Exception as e:
-            print(f"Unexpected error: {str(e)}")
-            raise HTTPException(status_code=500, detail="Internal Server Error")
+            raise HTTPException(status_code=500, detail=str(e))
+
 
 ai_talk = AiTalk()
 
@@ -58,52 +59,3 @@ async def chat(request: MessageRequest):
     print(f"Received message: {request.prompt}")
     reply = ai_talk.ai(request.prompt)
     return {"response": reply}
-
-
-# app = FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000","https://mooding-up.vercel.app"],  
-#     allow_credentials=True,
-#     allow_methods=["*"],  
-#     allow_headers=["*"],  
-# )
-
-# load_dotenv(dotenv_path=".env.local")
-# openai.api_key = os.getenv("CHATGPT_API_TOKEN")
-
-# class MessageRequest(BaseModel):
-#     prompt: str
-
-# system_message = {
-#     "role": "system",
-#     "content": "你是溫柔的輔導老師，請用一次單元型模型，來鼓勵與你談話的人，給他們方向和建議，每次談話不超過20個字。"
-# }
-
-
-# class AiTalk:
-#     def __init__(self):
-#         self.messages = [system_message]
-
-#     def ai(self, prompt: str) -> str:
-#         self.messages.append({"role": "user", "content": prompt})
-
-#         try:
-#             response = openai.ChatCompletion.create(
-#                 model="gpt-3.5-turbo",
-#                 messages=self.messages
-#             )
-#             reply = response.choices[0].message['content']
-#             self.messages.append({"role": "assistant", "content": reply})
-#             return reply
-#         except Exception as e:
-#             raise HTTPException(status_code=500, detail=str(e))
-
-
-# ai_talk = AiTalk()
-
-# @app.post("/chat/")
-# async def chat(request: MessageRequest):
-#     print(f"Received message: {request.prompt}")
-#     reply = ai_talk.ai(request.prompt)
-#     return {"response": reply}
