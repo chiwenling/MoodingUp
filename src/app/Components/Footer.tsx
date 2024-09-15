2aa6cdacfc16ea6bb8e72ff6948405f3f7809000
chiwenling <147138397+chiwenling@users.noreply.github.com>
2024-09-15 20:19:16 +0800

    footer調整

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[50px] bg-sisal-900 text-sisal-200 flex justify-center items-center text-[13px]">
+    <footer className="w-full h-[10vh] bg-sisal-900 text-sisal-200 flex justify-center items-center text-[13px]">
       <p>Copyright © 2024 wenling project.</p>
     </footer>
   );
